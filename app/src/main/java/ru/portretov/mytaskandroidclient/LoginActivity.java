92614c59af80241a3871ae70ce2645cb4d4fda0f
portretov-vs-170320 <portretovvitaly@gmail.com>
2017-11-21 15:27:07 +0300

    Частичное изменение дизайна Добавление первой Activity(с логином)

---
+++ app/src/main/java/ru/portretov/mytaskandroidclient/LoginActivity.java
@@ -0,0 +1,10 @@
+package ru.portretov.mytaskandroidclient;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adminvp on 11/21/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
