39fa63e9b61b4d951c294ab2f8efbabf0ae751d7
portretov-vs-170320 <portretovvitaly@gmail.com>
2017-11-24 03:25:37 +0300

    Подготовка к созданию Task'a (создание фрагментов и их layout'ов)

---
+++ app/src/main/java/ru/portretov/mytaskandroidclient/createtaskfragment/CreateTaskChapterThreeFragment.java
@@ -0,0 +1,22 @@
+package ru.portretov.mytaskandroidclient.createtaskfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.portretov.mytaskandroidclient.R;
+
+/**
+ * Created by adminvp on 11/24/17.
+ */
+
+public class CreateTaskChapterThreeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_task_chapter_3, null);
+    }
+}
