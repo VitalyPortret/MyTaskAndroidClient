e7ee3ec922ad64380c901c0b127e1736345ed3a4
portretov-vs-170320 <portretovvitaly@gmail.com>
2017-11-29 15:42:10 +0300

    Подключение библиотеки для парсинга JSON(FastJson), разрешаю доступ к интернету, добавил адрес для отправки таска

--- app/src/main/java/ru/portretov/mytaskandroidclient/util/ServerURL.java
+++ app/src/main/java/ru/portretov/mytaskandroidclient/util/ServerURL.java
@@ -4,8 +4,10 @@ package ru.portretov.mytaskandroidclient.util;
  * Created by adminvp on 11/29/17.
  */
 
-public class ServerURL {
+class ServerURL {
     private static final String URL_SERVER = "http://10.0.2.2:8080";
 
-    public static final String URL_POST_TASK = URL_SERVER + "/api/tasks/create";
+    static final String URL_POST_TASK = URL_SERVER + "/api/tasks/create";
+
+    static final String URL_ALL_TASKS = URL_SERVER + "/api/tasks/";
 }
