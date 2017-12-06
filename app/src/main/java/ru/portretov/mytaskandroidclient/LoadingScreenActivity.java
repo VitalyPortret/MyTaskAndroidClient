1f1b690be70a5459603db164cb4667615c86ccf6
portretov-vs-170320 <portretovvitaly@gmail.com>
2017-12-06 21:54:20 +0300

    Создание Загрузочного экрана(activity, layout) Добавление картинок

---
+++ app/src/main/java/ru/portretov/mytaskandroidclient/LoadingScreenActivity.java
@@ -0,0 +1,37 @@
+package ru.portretov.mytaskandroidclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adminvp on 12/6/17.
+ */
+
+public class LoadingScreenActivity extends AppCompatActivity {
+
+    private final int WELCOME_SCREEN_DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(WELCOME_SCREEN_DELAY);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(LoadingScreenActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
--- app/src/main/java/ru/portretov/mytaskandroidclient/LoginActivity.java
+++ app/src/main/java/ru/portretov/mytaskandroidclient/LoginActivity.java
@@ -1,20 +1,27 @@
 package ru.portretov.mytaskandroidclient;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by adminvp on 11/21/17.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnStart).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
     }
 }
