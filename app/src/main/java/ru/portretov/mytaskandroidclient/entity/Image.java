199f7c01b4b801fb39023902b7ea8124bbc944be
portretov-vs-170320 <portretovvitaly@gmail.com>
2017-12-06 23:58:38 +0300

    Создание классов, для пользователя и его фото

---
+++ app/src/main/java/ru/portretov/mytaskandroidclient/entity/Image.java
@@ -0,0 +1,18 @@
+package ru.portretov.mytaskandroidclient.entity;
+
+/**
+ * Created by adminvp on 12/6/17.
+ */
+
+public class Image {
+
+    private byte[] imageData;
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+}
---
+++ app/src/main/java/ru/portretov/mytaskandroidclient/entity/Profile.java
@@ -0,0 +1,126 @@
+package ru.portretov.mytaskandroidclient.entity;
+
+import java.util.Date;
+
+public class Profile {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String miniBio;
+
+    private String location;
+
+    private Date birthday;
+
+    private Integer age;
+
+    private boolean postTask;
+
+    private boolean earnMoney;
+
+    private boolean confirmed;
+
+    private Double creatorRating;
+
+    private Double executeRating;
+
+    private Image image;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiniBio() {
+        return miniBio;
+    }
+
+    public void setMiniBio(String miniBio) {
+        this.miniBio = miniBio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isPostTask() {
+        return postTask;
+    }
+
+    public void setPostTask(boolean postTask) {
+        this.postTask = postTask;
+    }
+
+    public boolean isEarnMoney() {
+        return earnMoney;
+    }
+
+    public void setEarnMoney(boolean earnMoney) {
+        this.earnMoney = earnMoney;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public Double getCreatorRating() {
+        return creatorRating;
+    }
+
+    public void setCreatorRating(Double creatorRating) {
+        this.creatorRating = creatorRating;
+    }
+
+    public Double getExecuteRating() {
+        return executeRating;
+    }
+
+    public void setExecuteRating(Double executeRating) {
+        this.executeRating = executeRating;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
